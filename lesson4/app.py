d6d53a34b0971e9fb493bbc2284fe0fd018a06f3
Morgado18 <guiandrademelo108@gmail.com>
2025-08-17 11:54:57 +0100

    cool

---
+++ lesson4/app.py
@@ -0,0 +1,6 @@
+
+string = "andrade de melo"
+
+print(string.isupper()) # .islower(), these functions check whether a string is in lowercase or uppercase letters
+
+print(string.replace(' de ', '-'))
---
+++ lesson5/app.py
@@ -0,0 +1,8 @@
+
+# inputs, comments & calculator
+
+
+
+name = input("Type your name: ")
+age = input(f"how old are you?: {name}")
+print(name)
