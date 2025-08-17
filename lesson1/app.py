7db5abaac10ed2db2397be3020a441ff92d5d2d0
Morgado18 <guiandrademelo108@gmail.com>
2025-08-17 10:09:23 +0100

    cool

---
+++ lesson1/app.py
@@ -0,0 +1,11 @@
+
+name = "Andrade"
+age = 12
+birth_date = "2006-03-04"
+
+# print("Hello world!")
+# print("I'm Morgado Andrade and I learning a new programming language 'python'")
+
+print("My name is: "+ name)
+print(f"I have {age} years old")
+
---
+++ lesson2/app.py
@@ -0,0 +1,8 @@
+
+# Working with numbers
+
+num = 2
+
+print(type(2))
+
+print(float(num))
